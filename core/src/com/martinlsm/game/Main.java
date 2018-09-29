6876748871147e6ddb1c0d91764d8c0f5496faf5
martinlsm <martin.lindstrom94@gmail.com>
2018-09-29 19:15:44 +0200

    Add square asset file.

--- core/src/com/martinlsm/game/Main.java
+++ core/src/com/martinlsm/game/Main.java
@@ -2,26 +2,25 @@ package com.martinlsm.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	Viewport viewport;
-	Camera cam;
+	OrthographicCamera cam;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("square.png");
 		cam = new OrthographicCamera();
-		viewport = new FitViewport(1920, 1080, cam);
+		viewport = new StretchViewport(1920, 1080, cam);
 		viewport.apply(true);
 	}
 
